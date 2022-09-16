4f2fff126a1e0a50c4e86c2481a86449739b85f2
Olivia Gautheron <oxtramag@hotmail.fr>
2022-09-16 16:14:19 +0200

    feat:add home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
     <nav className="header">
         <Link className="header__link" to="/">
             <img className='header__link__logo' src={logo} alt='Argent Bank Logo'/>
-            <h1 className='header__link__title'>Argent Bank</h1>
+            <h1 className='hide__only'>Argent Bank</h1>
         </Link>
         <Link className='header__link__icon' to="/signIn">
             <div className='link__icon__img'>{icon}</div>
